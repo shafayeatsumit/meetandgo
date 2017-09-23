a39d850e67df9f918d8da8205cf7fd4ae15dbaa3
shafayeatsumit <shafayeatkabir@gmail.com>
2017-09-23 11:35:44 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
+import { FontAwesome } from '@expo/vector-icons';
+
+import Router from './navigation/Router';
+import cacheAssetsAsync from './utilities/cacheAssetsAsync';
+export default class App extends React.Component {
+  state = {
+    appIsReady: true,
+  };
+
+  // componentWillMount() {
+  //   this._loadAssetsAsync();
+  // }
+
+  async _loadAssetsAsync() {
+    try {
+      await cacheAssetsAsync({
+        images: [require('./assets/images/expo-wordmark.png')],
+        fonts: [
+          FontAwesome.font,
+          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+        ],
+      });
+    } catch (e) {
+      console.warn(
+        'There was an error caching assets (see: main.js), perhaps due to a ' +
+          'network timeout, so we skipped caching. Reload the app to try again.'
+      );
+      console.log(e.message);
+    } finally {
+      this.setState({ appIsReady: true });
+    }
+  }
+
+  render() {
+    if (this.state.appIsReady) {
+      return (
+        <View style={styles.container}>
+          <NavigationProvider router={Router}>
+            <StackNavigation
+              id="root"
+              initialRoute={Router.getRoute('rootNavigation')}
+            />
+          </NavigationProvider>
+
+        </View>
+      );
+    } else {
+      return <Expo.AppLoading />;
+    }
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
